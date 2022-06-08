34586d306fa40f51347aae89167326d96208cf65
Bobby Calderwood <8336+bobby@users.noreply.github.com>
2022-06-08 00:09:03 -0600

    WIP Processor-based Topology

--- transactor/src/main/kotlin/com/evidentdb/transactor/topology.kt
+++ transactor/src/main/kotlin/com/evidentdb/transactor/topology.kt
@@ -5,15 +5,54 @@ import com.evidentdb.domain.*
 import com.evidentdb.kafka.partitionBySource
 import io.cloudevents.CloudEvent
 import org.apache.kafka.streams.KeyValue
-import org.apache.kafka.streams.StreamsBuilder
 import org.apache.kafka.streams.Topology
 import org.apache.kafka.streams.kstream.KStream
 import org.apache.kafka.streams.processor.StreamPartitioner
+import org.apache.kafka.streams.processor.api.*
 import java.util.*
 
 class EventSourceStreamPartitioner: StreamPartitioner<UUID, CloudEvent> {
     override fun partition(topic: String?, key: UUID?, value: CloudEvent?, numPartitions: Int): Int =
         partitionBySource(value!!, numPartitions)
+}
+
+class CommandProcessor:
+    ContextualProcessor<CommandId, CommandEnvelope, EventId, EventEnvelope>() {
+    private var transactor: Transactor = KafkaStreamsTransactor()
+
+    override fun init(context: ProcessorContext<EventId, EventEnvelope>?) {
+        super.init(context)
+        // transactor.init() w/ state stores
+    }
+
+    override fun close() {
+        super.close()
+        this.transactor = KafkaStreamsTransactor()
+    }
+
+    override fun process(record: Record<CommandId, CommandEnvelope>?) {
+        val command = record?.value() ?: throw IllegalStateException()
+        val event = when (command) {
+            is CreateDatabase -> transactor.handleCreateDatabase(command)
+            is DeleteDatabase -> transactor.handleDeleteDatabase(command)
+            is RenameDatabase -> transactor.handleRenameDatabase(command)
+            is TransactBatch -> transactor.handleTransactBatch(command)
+        }.getOrHandle { error ->
+            ErrorEnvelope(
+                EventId.randomUUID(),
+                command.id,
+                command.databaseId,
+                error
+            )
+        }
+        TODO()
+    }
+
+    companion object {
+        fun create(): CommandProcessor {
+            return CommandProcessor()
+        }
+    }
 }
 
 object Topology {
@@ -26,45 +65,31 @@ object Topology {
         streamEventIdsTopic: String,
         streamsTopic: String
     ): Topology {
-        val builder = StreamsBuilder()
-        val eventsOrErrors = buildCommandProcessor(
-            transactor, builder, commandTopic, eventTopic
+        val topology = Topology()
+        topology.addSource("commands", commandTopic)
+        topology.addProcessor(
+            "commandProcessor",
+            CommandProcessor::create,
+            "commands"
         )
-        val justEvents = eventsOrErrors.filterNot {_, event -> event is ErrorEnvelope }
-        val eventStreams = justEvents.split()
-            .branch { _, event -> event is DatabaseEvent }
-            .branch { _, event -> event is BatchEvent }
-            .defaultBranch()
-        buildCatalog(eventStreams["1"]!!, catalogTopic)
-        buildIndex(eventStreams["2"]!!, eventsTopic, streamEventIdsTopic, streamsTopic)
-        return builder.build()
+
+        TODO()
+//        val justEvents = eventsOrErrors.filterNot {_, event -> event is ErrorEnvelope }
+//        val eventStreams = justEvents.split()
+//            .branch { _, event -> event is DatabaseEvent }
+//            .branch { _, event -> event is BatchEvent }
+//            .defaultBranch()
+//        buildCatalog(eventStreams["1"]!!, catalogTopic)
+//        buildIndex(eventStreams["2"]!!, eventsTopic, streamEventIdsTopic, streamsTopic)
+//        return topology
     }
 
     private fun buildCommandProcessor(
         transactor: Transactor,
-        builder: StreamsBuilder,
+        topology: Topology,
         commandTopic: String,
         eventTopic: String
-    ): KStream<DatabaseId, EventEnvelope> {
-        val commands = builder.stream<DatabaseId, CommandEnvelope>(commandTopic)
-        val eventsOrErrors = commands
-            .mapValues { databaseId, command ->
-                when (command) {
-                    is CreateDatabase -> transactor.handleCreateDatabase(command)
-                    is DeleteDatabase -> transactor.handleDeleteDatabase(command)
-                    is RenameDatabase -> transactor.handleRenameDatabase(command)
-                    is TransactBatch -> transactor.handleTransactBatch(command)
-                }.getOrHandle { error ->
-                    ErrorEnvelope(
-                        EventId.randomUUID(),
-                        command.id,
-                        databaseId,
-                        error
-                    )
-                }
-            }
-        eventsOrErrors.to(eventTopic)
-        return eventsOrErrors!!
+    ) {
     }
 
     private fun buildCatalog(
@@ -75,11 +100,11 @@ object Topology {
             .groupByKey()
             ?.aggregate(
                 { EmptyDatabase() },
-                { databaseId, event, database: Database? ->
+                { databaseId, event, database: IDatabase? ->
                     when (event) {
                         is DatabaseCreated -> event.data.database
                         is DatabaseDeleted -> null
-                        is DatabaseRenamed -> DatabaseRecord(databaseId, event.data.newName)
+                        is DatabaseRenamed -> Database(databaseId, event.data.newName)
                         else -> database
                     }
                 }
@@ -105,7 +130,7 @@ object Topology {
         }
         eventStream.to(eventsTopic)
         val streamEventIdsStream = eventStream.map { _, evt ->
-            KeyValue(streamKey(evt.databaseId, evt.stream), evt.id)
+            KeyValue(buildStreamKey(evt.databaseId, evt.stream), evt.id)
         }
         streamEventIdsStream.to(streamEventIdsTopic)
 

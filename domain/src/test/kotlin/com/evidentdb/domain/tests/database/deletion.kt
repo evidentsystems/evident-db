db685d1c7452e236808db1164a0e93a46f1a30ed
Bobby Calderwood <8336+bobby@users.noreply.github.com>
2022-06-14 15:25:24 -0600

    More test infra tweaks

---
+++ domain/src/test/kotlin/com/evidentdb/domain/tests/database/deletion.kt
@@ -0,0 +1,16 @@
+package com.evidentdb.domain.tests.database
+
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+
+class DeletionTests {
+    @Test
+    fun `reject a database deletion proposal due to no database with name existing`() {
+        TODO("Not implemented yet")
+    }
+
+    @Test
+    fun `accept a database deletion proposal`() {
+        TODO("Not implemented yet")
+    }
+}

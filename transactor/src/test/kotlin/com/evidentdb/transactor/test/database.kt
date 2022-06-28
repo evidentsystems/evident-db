a59397cfaf2ccf9917cdce7c82275a7a332226da
Bobby Calderwood <8336+bobby@users.noreply.github.com>
2022-06-27 23:28:06 -0600

    Add database deletion test; refactor test helpers

--- transactor/src/test/kotlin/com/evidentdb/transactor/test/database.kt
+++ transactor/src/test/kotlin/com/evidentdb/transactor/test/database.kt
@@ -8,29 +8,7 @@ import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
 import java.util.*
 
-const val INTERNAL_COMMAND_TOPIC = "internal-commands"
-const val INTERNAL_EVENTS_TOPIC = "internal-events"
-const val DATABASES_TOPIC = "databases"
-const val DATABASE_NAMES_TOPIC = "database-names"
-const val BATCHES_TOPIC = "batches"
-const val STREAMS_TOPIC = "streams"
-const val EVENTS_TOPIC = "events"
-
 class TopologyTests {
-    private fun topology() =
-        Topology.build(
-            INTERNAL_COMMAND_TOPIC,
-            INTERNAL_EVENTS_TOPIC,
-            DATABASES_TOPIC,
-            DATABASE_NAMES_TOPIC,
-            BATCHES_TOPIC,
-            STREAMS_TOPIC,
-            EVENTS_TOPIC,
-        )
-
-    private fun driver(): TopologyTestDriver =
-        TopologyTestDriver(topology())
-
     @Test
     fun `topology creates a database`(): Unit =
         runBlocking {
@@ -88,6 +66,36 @@ class TopologyTests {
                 )
 
                 Assertions.assertEquals(it.databaseId, databaseNameStore.get(newName))
+
+                Assertions.assertTrue(
+                    driver.producedTopicNames().contains("databases")
+                )
+                Assertions.assertTrue(
+                    driver.producedTopicNames().contains("internal-events")
+                )
+                Assertions.assertTrue(
+                    driver.producedTopicNames().contains("database-names")
+                )
+            }
+        }
+
+    @Test
+    fun `topology deletes a database`(): Unit =
+        runBlocking {
+            val driver = driver()
+            val databaseStore = driver.getKeyValueStore<DatabaseId, Database>(Topology.DATABASE_STORE)
+            val databaseNameStore = driver.getKeyValueStore<DatabaseName, DatabaseId>(Topology.DATABASE_NAME_LOOKUP)
+            val service = TopologyTestDriverService(driver)
+            val databaseName = "foo"
+            service.createDatabase(databaseName)
+
+            val event = service.deleteDatabase(databaseName)
+
+            event.map {
+                val database = databaseStore.get(it.databaseId)
+                Assertions.assertNull(database)
+
+                Assertions.assertNull(databaseNameStore.get(databaseName))
 
                 Assertions.assertTrue(
                     driver.producedTopicNames().contains("databases")
--- transactor/src/test/kotlin/com/evidentdb/transactor/test/implementation.kt
+++ transactor/src/test/kotlin/com/evidentdb/transactor/test/implementation.kt
@@ -12,6 +12,28 @@ import com.evidentdb.transactor.Topology
 import org.apache.kafka.common.serialization.Serdes
 import org.apache.kafka.streams.TopologyTestDriver
 
+const val INTERNAL_COMMAND_TOPIC = "internal-commands"
+const val INTERNAL_EVENTS_TOPIC = "internal-events"
+const val DATABASES_TOPIC = "databases"
+const val DATABASE_NAMES_TOPIC = "database-names"
+const val BATCHES_TOPIC = "batches"
+const val STREAMS_TOPIC = "streams"
+const val EVENTS_TOPIC = "events"
+
+fun topology() =
+    Topology.build(
+        INTERNAL_COMMAND_TOPIC,
+        INTERNAL_EVENTS_TOPIC,
+        DATABASES_TOPIC,
+        DATABASE_NAMES_TOPIC,
+        BATCHES_TOPIC,
+        STREAMS_TOPIC,
+        EVENTS_TOPIC,
+    )
+
+fun driver(): TopologyTestDriver =
+    TopologyTestDriver(topology())
+
 class TopologyTestDriverCommandBroker(
     driver: TopologyTestDriver
 ): CommandBroker {

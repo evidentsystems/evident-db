b1246801a54bada8b94cc591f46cd933512e7a2b
Bobby Calderwood <8336+bobby@users.noreply.github.com>
2022-07-06 15:45:30 -0600

    Get service protobuf/grpc build infra working

--- service/build.gradle.kts
+++ service/build.gradle.kts
@@ -1,5 +1,8 @@
+import com.google.protobuf.gradle.*
+
 plugins {
     alias(libs.plugins.kotlin.jvm)
+    alias(libs.plugins.protobuf)
 }
 
 group = "com.evidentdb"
@@ -13,6 +16,37 @@ dependencies {
     implementation(project(":adapters"))
 
     implementation(libs.kafka.streams)
+
+    implementation(libs.protobuf.java)
+    implementation(libs.grpc.stub)
+    implementation(libs.grpc.kotlin.stub)
+    implementation(libs.grpc.protobuf)
+}
+
+protobuf {
+    plugins {
+        id("grpc")   { artifact = "io.grpc:protoc-gen-grpc-java:${libs.versions.grpc.get()}" }
+        id("grpckt") { artifact = "io.grpc:protoc-gen-grpc-kotlin:${libs.versions.grpckotlin.get()}:jdk8@jar" }
+    }
+    generateProtoTasks {
+        ofSourceSet("main").forEach {
+            it.plugins {
+                // Apply the "grpc" plugin whose spec is defined above, without options.
+                id("grpc")
+                id("grpckt")
+            }
+        }
+    }
+}
+
+sourceSets {
+    main {
+        java {
+            srcDirs("build/generated/source/proto/main/grpc")
+            srcDirs("build/generated/source/proto/main/grpckt")
+            srcDirs("build/generated/source/proto/main/java")
+        }
+    }
 }
 
 tasks.test {
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -11,7 +11,11 @@ dependencyResolutionManagement {
             version("kotlin", "1.6.21")
             version("kafka", "3.2.0")
             version("cloudevents", "2.3.0")
+            version("protobuf-java", "3.21.2")
+            version("grpc", "1.47.0")
+            version("grpckotlin", "1.3.0")
             version("junit", "5.6.0")
+
             plugin("protobuf", "com.google.protobuf").version("0.8.18")
             plugin("kotlin-jvm", "org.jetbrains.kotlin.jvm").versionRef("kotlin")
             library("kotlinx-coroutines", "org.jetbrains.kotlinx", "kotlinx-coroutines-core").version("1.6.2")
@@ -21,7 +25,10 @@ dependencyResolutionManagement {
             library("cloudevents-api", "io.cloudevents", "cloudevents-api").versionRef("cloudevents")
             library("cloudevents-kafka", "io.cloudevents", "cloudevents-kafka").versionRef("cloudevents")
             library("cloudevents-protobuf", "io.cloudevents", "cloudevents-protobuf").versionRef("cloudevents")
-            library("protobuf-java", "com.google.protobuf", "protobuf-java").version("3.21.2")
+            library("protobuf-java", "com.google.protobuf", "protobuf-java").versionRef("protobuf-java")
+            library("grpc-stub", "io.grpc", "grpc-stub").versionRef("grpc")
+            library("grpc-kotlin-stub", "io.grpc", "grpc-kotlin-stub").versionRef("grpckotlin")
+            library("grpc-protobuf", "io.grpc", "grpc-protobuf").versionRef("grpc")
             library("junit-api", "org.junit.jupiter", "junit-jupiter-api").versionRef("junit")
             library("junit-engine", "org.junit.jupiter", "junit-jupiter-engine").versionRef("junit")
         }

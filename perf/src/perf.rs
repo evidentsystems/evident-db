e90b43e43b58fa14ed48bb4a3ee263729d5a9a32
Bobby Calderwood <8336+bobby@users.noreply.github.com>
2022-08-04 11:58:09 -0600

    Refactor client test

--- perf/src/perf.rs
+++ perf/src/perf.rs
@@ -14,27 +14,61 @@ pub mod io {
 
 use com::evidentdb::evident_db_client::EvidentDbClient;
 use com::evidentdb::*;
+use std::time::Instant;
+
+const OLD_NAME: &str = "my-old-database";
+const NEW_NAME: &str = "my-new-database";
+const DB_URL: &str = "http://[::1]:50051";
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let mut client = EvidentDbClient::connect("http://[::1]:50051").await?;
+    let mut client = EvidentDbClient::connect(DB_URL).await?;
+    create_database(&mut client).await?;
+    rename_database(&mut client).await?;
+    delete_database(&mut client).await?;
 
+    Ok(())
+}
+
+async fn create_database(client: &mut EvidentDbClient<tonic::transport::Channel>) -> Result<(), Box<dyn std::error::Error>> {
     let request = tonic::Request::new(DatabaseCreationInfo {
-        name: "my-new-database".into(),
+        name: OLD_NAME.into(),
     });
 
+    let start = Instant::now();
     let response = client.create_database(request).await?;
+    let duration = start.elapsed();
 
-    println!("RESPONSE={:?}", response);
+    println!("LATENCY={:?} RESPONSE={:?}", duration, response);
 
+    Ok(())
+}
+
+async fn rename_database(client: &mut EvidentDbClient<tonic::transport::Channel>) -> Result<(), Box<dyn std::error::Error>> {
     let request = tonic::Request::new(DatabaseRenameInfo {
-        old_name: "my-new-database".into(),
-        new_name: "my-other-database".into(),
+        old_name: OLD_NAME.into(),
+        new_name: NEW_NAME.into(),
     });
 
+    let start = Instant::now();
     let response = client.rename_database(request).await?;
+    let duration = start.elapsed();
 
-    println!("RESPONSE={:?}", response);
+    println!("LATENCY={:?} RESPONSE={:?}", duration, response);
+
+    Ok(())
+}
+
+async fn delete_database(client: &mut EvidentDbClient<tonic::transport::Channel>) -> Result<(), Box<dyn std::error::Error>> {
+    let request = tonic::Request::new(DatabaseDeletionInfo {
+        name: NEW_NAME.into(),
+    });
+
+    let start = Instant::now();
+    let response = client.delete_database(request).await?;
+    let duration = start.elapsed();
+
+    println!("LATENCY={:?} RESPONSE={:?}", duration, response);
 
     Ok(())
 }

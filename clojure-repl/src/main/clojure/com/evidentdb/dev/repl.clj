d2c11296383cfcf777cb9b37c9baf6d4a574d0d7
Bobby Calderwood <8336+bobby@users.noreply.github.com>
2022-10-29 21:18:04 -0600

    Add ability to examine RocksDB from Clojure REPL

--- clojure-repl/src/main/clojure/com/evidentdb/dev/repl.clj
+++ clojure-repl/src/main/clojure/com/evidentdb/dev/repl.clj
@@ -1,8 +1,10 @@
 (ns com.evidentdb.dev.repl
+  (:require [clojure.string :as string]
+            [clj-rocksdb :as rocksdb])
   (:import [java.net URI]
            [io.grpc ManagedChannelBuilder]
            [io.cloudevents CloudEventData]
-           [com.evidentdb.client EvidentDB StreamState StreamState$Any CloseableIterator]))
+           [com.evidentdb.client EvidentDB StreamState StreamState$Any CloseableIterator CodecKt]))
 
 (defn event-proposal
   ([event-type stream-name]
@@ -43,8 +45,7 @@
   (def database-name "clojure-repl")
 
   (def catalog
-    (with-open [catalog-iter (.catalog client)]
-      (into [] (iterator-seq catalog-iter))))
+    (eagerize (.catalog client)))
 
   (.createDatabase client database-name)
 
@@ -58,7 +59,8 @@
 
   db1
 
-  (def batch [(event-proposal "event.occurred" "my-stream")])
+  (def batch [(event-proposal "event.occurred" "my-stream")
+              (event-proposal "event.happened" "another-my-stream")])
 
   (def batch-result @(.transact conn batch))
 
@@ -68,13 +70,48 @@
 
   (= db2 db3)
 
-  (count (eagerize (.stream db2 "my-stream")))
+  (time (eagerize (.stream db1 "another-my-stream")))
 
-  (count (eagerize (.log conn)))
+  (time (eagerize (.log conn)))
 
   (.deleteDatabase client database-name)
 
   (.shutdownNow client)
 
+  (def db-prefix "/home/bobby/code/evidentsystems/evident-db/app/data/transactor/evidentdb-default-tenant-transactor/0_1/rocksdb/")
+
+  (defn encode-string
+    [s]
+    (.getBytes s "utf-8"))
+
+  (defn decode-string
+    [b]
+    (String. b "utf-8"))
+
+  (defn connect
+    [dir]
+    (rocksdb/create-db (str db-prefix "/" dir)
+                       {:create-if-missing? false
+                        :key-encoder encode-string
+                        :key-decoder decode-string}))
+
+  (def events (connect "EVENT_STORE"))
+  (def batches (connect "BATCH_STORE"))
+
+  (rocksdb/get events (str "clojure-repl/" (CodecKt/longToBase32HexString 4)))
+
+  (def event-seq (rocksdb/iterator events "clojure-repl/"))
+  (def batches-seq (rocksdb/iterator batches))
+
+  (count batches-seq)
+
+  (count (map (comp #(CodecKt/base32HexStringToLong %)
+                    last
+                    #(string/split % #"/")
+                    first)
+              i))
+
+  (.close events)
+  (.close batches)
   ;; end sample usage
   )

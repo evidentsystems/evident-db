e3ba34b745836865a280ba95ee4e3c6317ca4e67
Bobby Calderwood <8336+bobby@users.noreply.github.com>
2023-09-19 11:56:20 -0500

    Upgrade to Micronaut 4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,11 +1,12 @@
 plugins {
     id("org.jetbrains.kotlin.jvm")
-    id("org.jetbrains.kotlin.kapt")
-    id("com.github.johnrengelman.shadow") version "7.1.1"
-    id("io.micronaut.application") version "3.5.0"
+    id("com.google.devtools.ksp") version "1.8.22-1.0.11"
+    id("com.github.johnrengelman.shadow") version "8.1.1"
+    id("io.micronaut.application") version "4.1.1"
+    id("io.micronaut.aot") version "4.1.1"
 }
 
-version = "0.1.0-SNAPSHOT"
+version = "0.1.0-alpha-SNAPSHOT"
 group = "com.evidentdb"
 
 dependencies {
@@ -21,15 +22,17 @@ dependencies {
     implementation(libs.grpc.kotlin.stub)
 
     implementation("info.picocli:picocli")
-    implementation("io.micronaut:micronaut-runtime")
+    compileOnly("io.micronaut:micronaut-runtime")
     implementation("io.micronaut.grpc:micronaut-grpc-server-runtime")
+    implementation("io.micronaut:micronaut-jackson-databind") // TODO: decide between Jackson or Micronaut Serde
     implementation("io.micronaut.kotlin:micronaut-kotlin-runtime")
     implementation("jakarta.annotation:jakarta.annotation-api")
-    implementation("io.micronaut:micronaut-validation")
+    implementation("io.micronaut.validation:micronaut-validation")
     implementation("io.micronaut.micrometer:micronaut-micrometer-registry-prometheus")
     implementation("io.micronaut:micronaut-management")
     implementation("io.micronaut:micronaut-http-server-netty")
     implementation("io.micronaut.kafka:micronaut-kafka")
+    runtimeOnly("org.yaml:snakeyaml")
     runtimeOnly("ch.qos.logback:logback-classic")
 
     testImplementation("io.micronaut:micronaut-http-client")
@@ -45,16 +48,37 @@ kotlin {
     }
 }
 
+application {
+    mainClass.set("com.evidentdb.app.CliKt")
+    applicationDefaultJvmArgs = listOf("-Xmx4g", "-Xms4g")
+}
+
+graalvmNative.toolchainDetection = false
 micronaut {
     version.set(libs.versions.micronaut)
+    runtime("netty")
     testRuntime("junit5")
     processing {
         incremental(true)
         annotations("com.evidentdb.*")
     }
+    aot {
+        // Please review carefully the optimizations enabled below
+        // Check https://micronaut-projects.github.io/micronaut-aot/latest/guide/ for more details
+        optimizeServiceLoading = false
+        convertYamlToJava = false
+        precomputeOperations = true
+        cacheEnvironment = true
+        optimizeClassLoading = true
+        deduceEnvironment = true
+        optimizeNetty = true
+    }
 }
 
-application {
-    mainClass.set("com.evidentdb.app.CliKt")
-    applicationDefaultJvmArgs = listOf("-Xmx4g", "-Xms4g")
+graalvmNative {
+    binaries {
+        named("main") {
+            imageName = "evident-db"
+        }
+    }
 }

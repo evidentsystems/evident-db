cd033439700532a1b426fafc6260f7446894836a
Bobby Calderwood <8336+bobby@users.noreply.github.com>
2024-01-25 17:08:35 -0600

    Minor tweaks to build-logic for micronaut lib/app

--- build-logic/micronaut-app/src/main/kotlin/com.evidentdb.build.micronaut-app.gradle.kts
+++ build-logic/micronaut-app/src/main/kotlin/com.evidentdb.build.micronaut-app.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     id("com.evidentdb.build.kotlin")
-    id("com.google.devtools.ksp")
     id("org.jetbrains.kotlin.plugin.allopen")
+    id("com.google.devtools.ksp")
     id("com.github.johnrengelman.shadow")
     id("io.micronaut.application")
     id("io.micronaut.aot")
@@ -9,14 +9,14 @@ plugins {
 
 dependencies {
     ksp("io.micronaut:micronaut-inject-kotlin")
+    ksp("io.micronaut.serde:micronaut-serde-processor")
     implementation("io.micronaut.kotlin:micronaut-kotlin-runtime")
     implementation("io.micronaut.serde:micronaut-serde-jackson")
-    implementation("jakarta.annotation:jakarta.annotation-api")
-    runtimeOnly("org.yaml:snakeyaml")
     runtimeOnly("ch.qos.logback:logback-classic")
     runtimeOnly("com.fasterxml.jackson.module:jackson-module-kotlin")
 
     kspTest("io.micronaut:micronaut-inject-kotlin")
+    kspTest("io.micronaut.serde:micronaut-serde-processor")
     testImplementation("io.micronaut:micronaut-http-client")
     testImplementation("io.micronaut.test:micronaut-test-junit5")
 }
--- build-logic/micronaut-lib/src/main/kotlin/com.evidentdb.build.micronaut-lib.gradle.kts
+++ build-logic/micronaut-lib/src/main/kotlin/com.evidentdb.build.micronaut-lib.gradle.kts
@@ -1,11 +1,14 @@
 plugins {
     id("com.evidentdb.build.kotlin")
-    id("com.google.devtools.ksp")
     id("org.jetbrains.kotlin.plugin.allopen")
+    id("com.google.devtools.ksp")
     id("io.micronaut.library")
 }
 
 dependencies {
     ksp("io.micronaut:micronaut-inject-kotlin")
+    ksp("io.micronaut.serde:micronaut-serde-processor")
+
     kspTest("io.micronaut:micronaut-inject-kotlin")
+    kspTest("io.micronaut.serde:micronaut-serde-processor")
 }

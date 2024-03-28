bbe212e8501c64e007311b6e2c68a65865ae6bad
Bobby Calderwood <8336+bobby@users.noreply.github.com>
2024-03-27 21:04:53 -0500

    Bump platform deps

--- build-logic/micronaut-app/build.gradle.kts
+++ build-logic/micronaut-app/build.gradle.kts
@@ -4,9 +4,9 @@ plugins {
 
 dependencies {
     implementation(project(":kotlin"))
-    implementation("org.jetbrains.kotlin:kotlin-allopen:1.9.22")
-    implementation("com.google.devtools.ksp:com.google.devtools.ksp.gradle.plugin:1.9.22-1.0.16")
+    implementation("org.jetbrains.kotlin:kotlin-allopen:1.9.23")
+    implementation("com.google.devtools.ksp:com.google.devtools.ksp.gradle.plugin:1.9.23-1.0.19")
     implementation("com.github.johnrengelman:shadow:8.1.1")
-    implementation("io.micronaut.gradle:micronaut-gradle-plugin:4.2.1")
-    implementation("io.micronaut.gradle:micronaut-aot-plugin:4.2.1")
+    implementation("io.micronaut.gradle:micronaut-gradle-plugin:4.3.4")
+    implementation("io.micronaut.gradle:micronaut-aot-plugin:4.3.4")
 }
--- build-logic/micronaut-lib/build.gradle.kts
+++ build-logic/micronaut-lib/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 
 dependencies {
     implementation(project(":kotlin"))
-    implementation("org.jetbrains.kotlin:kotlin-allopen:1.9.22")
-    implementation("com.google.devtools.ksp:com.google.devtools.ksp.gradle.plugin:1.9.22-1.0.16")
-    implementation("io.micronaut.gradle:micronaut-gradle-plugin:4.2.1")
+    implementation("org.jetbrains.kotlin:kotlin-allopen:1.9.23")
+    implementation("com.google.devtools.ksp:com.google.devtools.ksp.gradle.plugin:1.9.23-1.0.19")
+    implementation("io.micronaut.gradle:micronaut-gradle-plugin:4.3.4")
 }
